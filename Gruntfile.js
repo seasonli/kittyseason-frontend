12b5ea517d07d7806c28568a6ecf011e12b83b58
筷子 <season.chopsticks@gmail.com>
2016-03-22 00:21:43 +0800

    Config for debug watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,7 +107,6 @@ module.exports = function (grunt) {
           'static/**/*.*'
         ],
         tasks: [
-        a
           'htmlbuild:dev',
           'webpack:common',
           'copy:common'
