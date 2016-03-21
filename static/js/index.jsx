53ed0311cc65bec35252f48087c43642b962bc81
筷子 <season.chopsticks@gmail.com>
2016-03-21 19:30:13 +0800

    Replace jquery with zepto

--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,16 +1,2 @@
 var React = require('react/addons');
 var $ = require('npm-zepto');
-var Mui = require('material-ui');
-
-
-React.render(
-  <Mui.AppBar
-    title="kittyseason" 
-    iconElementLeft={
-      <Mui.IconButton>
-        <Mui.FontIcon className="muidocs-icon-action-home" />
-      </Mui.IconButton>
-    }
-  />,
-  $('.header-wrapper').get(0)
-);
