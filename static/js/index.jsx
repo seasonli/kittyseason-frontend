d9af9ae41511de363b47084f0869cd2beec262d7
seasonli <season.chopsticks@gmail.com>
2016-03-22 22:49:23 +0800

    Format code

--- static/js/index.jsx
+++ static/js/index.jsx
@@ -11,57 +11,57 @@ FeedUi.FeedDialog = React.createClass({
       open: this.props.open || false
     };
   },
-  render: function () {
+  render: function() {
     var actions = [
       <Mui.FlatButton
         label="取消"
-        primary={true}
-      />,
+        primary={true} />,
       <Mui.FlatButton
         label="添加"
-        primary={true}
-      />
+        primary={true} />
     ];
 
-    return ( <Mui.Dialog
+    return (
+      <Mui.Dialog
         title="添加一条记录"
-        modal={
-          true
-        } 
-        open={
-          this.state.open
-        }
-        actions={
-          actions
-        }
-        >
-      添加一条记录</Mui.Dialog>
-    );
+        modal={true}
+        open={this.state.open}
+        actions={actions}>
+        添加一条记录
+      </Mui.Dialog>
+      );
   },
-  handleOpen: function () {
-    this.setState({open: true});
+  handleOpen: function() {
+    this.setState({
+      open: true
+    });
   }
 });
 
 
 // Entrance
-var header = React.render( < Mui.AppBar title = "kittyseason"
-  iconElementLeft={
-    <Mui.IconButton>
-      <Mui.FontIcon className="muidocs-icon-action-home" />
-    </Mui.IconButton>
-  }
-  iconElementRight={
-    <Mui.FlatButton label="New"
-      onClick={
-        function () {
-          feeddialog.handleOpen();
-        }
-      }
-    />
-  }
-  />,
+var header = React.render(
+  <Mui.AppBar
+    title="kittyseason"
+    iconElementLeft={<Mui.IconButton>
+                       <Mui.FontIcon className="muidocs-icon-action-home" />
+                     </Mui.IconButton>}
+    iconElementRight={<Mui.FlatButton
+                        label="New"
+                        onClick={feeddialog.handleOpen} />} />,
   $('.header-wrapper').get(0)
 );
 
 var feeddialog = React.render(<FeedUi.FeedDialog />, $('.dialog-wrapper').get(0));
+
+
+
+
+
+
+
+
+
+
+
+//
